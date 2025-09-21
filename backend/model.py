bd05803df6909a5a732fa46701d8835d7830a0ee
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-09-21 13:44:40 +0530

    created the classification model

--- backend/model.py
+++ backend/model.py
@@ -3,11 +3,28 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
+import pickle 
 #importing dataset
-df = pd.read_csv('data/Iris.csv')
+df = pd.read_csv('backend/data/iris.csv')
 
 #This is how the data is organized... see the first 5 tuples of the dataset
-df.head()
+print(df.head())
 
-#x is independent and Y is dependent variable 
+#Now initializing dependent and independent variables 
+X = df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = df['Species']
+
+#Splitting the datset in train test split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
+
+#Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Instanciating the model 
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+
+#Making a pickle file of the model
+pickle.dump(classifier, open('model.pkl', 'wb'))
